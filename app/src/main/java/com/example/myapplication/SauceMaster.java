5a26bbf08eba8f402158de08f1de385de81b6dd9
Sakadyk Wenut Stifferson <sakadyk69@gmail.com>
2023-07-13 01:15:06 +0700

    Some minor button tweak

--- app/src/main/java/com/example/myapplication/SauceMaster.java
+++ app/src/main/java/com/example/myapplication/SauceMaster.java
@@ -6,18 +6,22 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SauceMaster extends AppCompatActivity {
     EditText code;
-    RelativeLayout button;
-    ImageView clear_code;
+    RelativeLayout button, inputFields;
+    ImageView clearCode;
     MediaPlayer mediaPlayer;
     AudioManager audioManager;
     AudioManager.OnAudioFocusChangeListener audioFocusChangeListener;
@@ -30,7 +34,8 @@ public class SauceMaster extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         code = findViewById(R.id.code_input);
-        clear_code = findViewById(R.id.clear_icon);
+        clearCode = findViewById(R.id.clear_icon);
+        inputFields = findViewById(R.id.input_fields);
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
 
         // Initialize MediaPlayer and set looping
@@ -52,31 +57,63 @@ public class SauceMaster extends AppCompatActivity {
             }
         };
 
-        button.setOnClickListener(new View.OnClickListener() {
+        code.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View view) {
-                String codeInput = code.getText().toString();
-                String url;
-                if (TextUtils.isEmpty(codeInput)) {
-                    url = "https://nhentai.net/";
-                } else {
-                    url = "https://nhentai.net/g/" + codeInput;
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_GO || i == EditorInfo.IME_ACTION_DONE) {
+                    performButtonClick();
+                    return true;
                 }
-                Intent intent = new Intent(view.getContext(), Browser.class);
-                intent.putExtra("url", url);
-                intent.putExtra("previousActivity", Genshin.class.getName());
-                view.getContext().startActivity(intent);
-                releaseMediaPlayer();
+                return false;
             }
         });
 
-        clear_code.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performButtonClick();
+            }
+        });
+
+        clearCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Clear the text of uid_gi EditText
                 code.setText("");
             }
         });
+    }
+
+    private void performButtonClick() {
+        String codeInput = code.getText().toString();
+        String url;
+        if (TextUtils.isEmpty(codeInput)) {
+            url = "https://nhentai.net/";
+        } else {
+            url = "https://nhentai.net/g/" + codeInput;
+        }
+        Intent intent = new Intent(code.getContext(), Browser.class);
+        intent.putExtra("url", url);
+        intent.putExtra("previousActivity", Genshin.class.getName());
+        code.getContext().startActivity(intent);
+        releaseMediaPlayer();
+    }
+
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        View view = getCurrentFocus();
+        if (view != null && event.getAction() == MotionEvent.ACTION_DOWN) {
+            int[] coordinates = new int[2];
+            view.getLocationOnScreen(coordinates);
+            float x = event.getRawX() + view.getLeft() - coordinates[0];
+            float y = event.getRawY() + view.getTop() - coordinates[1];
+
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom()) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                view.clearFocus();
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 
     @Override
